d8bd4190d3cf77c97e1053059dd81f075a1bed3b
Michelle Gomes <michelle.gomes@MacBook-Pro-de-Michelle.local>
2021-03-31 21:21:01 -0300

    bases

--- commons/ui/src/main/java/com/example/ui/bindings/RecyclerViewBinding.kt
+++ commons/ui/src/main/java/com/example/ui/bindings/RecyclerViewBinding.kt
@@ -2,6 +2,7 @@ package com.example.ui.bindings
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ui.recyclerview.RecyclerViewItemDecoration
 
 @BindingAdapter("itemDecorationSpacing")
 fun RecyclerView.setItemDecorationSpacing(spacingPx: Float) {
